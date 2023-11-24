565a0a1a1b8ef90489415645c7ef59383539b121
Starinin Andrey <starininandrey@gmail.com>
2023-11-24 21:18:32 +0300

    Update

--- AuthApp/Program.cs
+++ AuthApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AuthApp;
+
+var userOK = new User()
+{
+    Login = "user",
+    Password = "12345"
+};
+
+var userBad = new User()
+{
+    Login = "admin",
+    Password = "123"
+};
+
+var auth = new LoggerAuth(new LogToConsole());
+
+Console.WriteLine(auth.Auth(userOK) ? "OK" : "ERROR");
+Console.WriteLine(auth.Auth(userBad) ? "OK" : "ERROR");
